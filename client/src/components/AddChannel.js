6f97cf57c296d3359f2ff1f4806695f42afdedd8
unknown <omardamak1@gmail.com>
2018-01-11 15:58:37 +0100

    Use optimistic UI

--- client/src/components/AddChannel.js
+++ client/src/components/AddChannel.js
@@ -9,13 +9,28 @@ const AddChannel = ({ mutate }) => {
       evt.persist();
       mutate({ 
         variables: { name: evt.target.value },
-        refetchQueries: [ { query: channelsListQuery }],
+        optimisticResponse: {
+          addChannel: {
+            name: evt.target.value,
+            id: Math.round(Math.random() * -1000000),
+            __typename: 'Channel',
+          },
+        },
+        update: (store, { data: { addChannel } }) => {
+            // Read the data from the cache for this query.
+            const data = store.readQuery({query: channelsListQuery });
+            // Add our channel from the mutation to the end.
+            data.channels.push(addChannel);
+            // Write the data back to the cache.
+            store.writeQuery({ query: channelsListQuery, data });
+          },
       })
       .then( res => {
         evt.target.value = '';  
       });
     }
   };
+
 
   return (
     <input
